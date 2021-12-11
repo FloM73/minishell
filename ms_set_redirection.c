09ad820e317509d2f1424d740cc0bab4422c2f1f
flormich <flormich@student.42wolfsburg.de>
2021-12-11 18:40:39 +0100

    echo anders behandetl

--- ms_set_redirection.c
+++ ms_set_redirection.c
@@ -6,7 +6,7 @@
 /*   By: flormich <flormich@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 11:52:27 by flormich          #+#    #+#             */
-/*   Updated: 2021/12/11 15:19:05 by flormich         ###   ########.fr       */
+/*   Updated: 2021/12/11 18:38:04 by flormich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void	set_red_shell(t_struct *st, int which_cmd, int *next_fd)
 	else
 	{
 		dup2(st->fd[READ], STDIN_FILENO);
-		if (which_cmd == st->nb_cmd - 1 || st->arr[which_cmd].fd_out != 1)
+		if (which_cmd == st->nb_cmd - 1)
 			dup2(st->arr[which_cmd].fd_out, STDOUT_FILENO);
 		else
 			dup2(next_fd[WRITE], STDOUT_FILENO);
@@ -42,8 +42,10 @@ void	set_red_shell(t_struct *st, int which_cmd, int *next_fd)
 
 int	launch_builtin(t_struct *st)
 {
-	if (st->tr + 1 == st->nb_cmd || st->arr[st->tr].f_ptr != &run_echo)
-		dup2(st->arr[st->tr].fd_out, st->fd[WRITE]);
+	if (st->arr[st->tr].f_ptr == &run_echo
+		&& st->tr + 1 != st->nb_cmd && st->arr[st->tr + 1].f_ptr == &run_echo)
+		return (0);
+	dup2(st->arr[st->tr].fd_out, st->fd[WRITE]);
 	st->res = st->arr[st->tr].f_ptr(st, &(st->arr[st->tr]));
 	return (0);
 }
